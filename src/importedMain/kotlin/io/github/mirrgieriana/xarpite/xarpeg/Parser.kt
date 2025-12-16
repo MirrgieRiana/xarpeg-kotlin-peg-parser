c389053c1474a9cf2452446c880350be3323824d
_ <tacticsrealize2@gmail.com>
2025-12-16 10:40:57 +0900

    Refactor parser to use Result for error handling
    Changed parser methods to return Result instead of throwing exceptions directly. Added parseAllOrNull and updated parseAllOrThrow to use the new Result-based parseAll, improving error handling and composability.

--- src/importedMain/kotlin/io/github/mirrgieriana/xarpite/xarpeg/Parser.kt
+++ src/importedMain/kotlin/io/github/mirrgieriana/xarpite/xarpeg/Parser.kt
@@ -66,12 +66,16 @@ class UnmatchedInputParseException(message: String, context: ParseContext, posit
 
 class ExtraCharactersParseException(message: String, context: ParseContext, position: Int) : ParseException(message, context, position)
 
-fun <T : Any> Parser<T>.parseAllOrThrow(src: String, useMemoization: Boolean = true): T {
+fun <T : Any> Parser<T>.parseAllOrThrow(src: String, useMemoization: Boolean = true) = this.parseAll(src, useMemoization).getOrThrow()
+
+fun <T : Any> Parser<T>.parseAllOrNull(src: String, useMemoization: Boolean = true) = this.parseAll(src, useMemoization).getOrNull()
+
+fun <T : Any> Parser<T>.parseAll(src: String, useMemoization: Boolean = true): Result<T> {
     val context = ParseContext(src, useMemoization)
-    val result = this.parseOrNull(context, 0) ?: throw UnmatchedInputParseException("Failed to parse.", context, 0)
+    val result = this.parseOrNull(context, 0) ?: return Result.failure(UnmatchedInputParseException("Failed to parse.", context, 0))
     if (result.end != src.length) {
         val string = src.drop(result.end).truncate(10, "...").escapeDoubleQuote()
-        throw ExtraCharactersParseException("""Extra characters found after position ${result.end}: "$string"""", context, result.end)
+        return Result.failure(ExtraCharactersParseException("""Extra characters found after position ${result.end}: "$string"""", context, result.end))
     }
-    return result.value
+    return Result.success(result.value)
 }
